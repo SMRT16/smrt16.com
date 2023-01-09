255fae53a00573c8577c03320456965aff381f84
Maxim Vasilkov <dev@smrt16.com>
2023-01-10 01:22:28 +0200

    oops

--- components/Utils/user.js
+++ components/Utils/user.js
@@ -1,12 +1,5 @@
-import useSWR from "swr";
-
-const fetcher = (url) => fetch(url).then((res) => res.json());
 
 
-export const getUser = async (uid)=>{
-    
-    return {};
-}
 
 
 export const saveDBRecord = async (uid)=>{
