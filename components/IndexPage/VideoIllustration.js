d8a6e2378d045c61a3cbdb8e576b5e4bda15fef1
Maxim Vasilkov <dev@smrt16.com>
2023-01-12 17:02:54 +0200

    temporaly replaceed missing video

--- components/IndexPage/VideoIllustration.js
+++ components/IndexPage/VideoIllustration.js
@@ -1,4 +1,4 @@
-import { Card } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
 import HoverVideoPlayer from "react-hover-video-player";
 
 export default function VideoIllustration(props) {
@@ -7,7 +7,7 @@ export default function VideoIllustration(props) {
     return (
         <div {...otherProps} className="videoIllustration">
 
-                <HoverVideoPlayer videoSrc={[
+                {/* <HoverVideoPlayer videoSrc={[
                     {src:"/SMRT16-index.webm",type:"video/webm"},
                     {src:"/SMRT16-index.mp4",type:"video/mp4"}
                 ]}
@@ -23,8 +23,23 @@ export default function VideoIllustration(props) {
                       }}
                     />
                   }
-                />
+                /> */}
 
+                <h4>Main points to remember!</h4>
+                <ul>
+                  <li>Spend USDT, receive USDT</li>
+                  <li>It's Polygon main net USDT</li>
+                  <li>You earn with the referral program</li>
+                  <li>Money from token sale goes for referral program payments</li>
+                  <li>No rules change, no price change, all is constant</li>
+                  <li>There is no minimum or maximum amount to come with</li>
+                  <li>But maximum amount you receive is limited to your SMRT16 balance</li>
+                  <li>You spend SMRT16 by receiving referral rewards</li>
+                </ul>
+                <div style={{margin:"auto",textAlign:"center"}}>
+                  <Button as="a" href="/0">LET'S GET STARTED</Button>
+                </div>
+                
         </div>
         
     );
