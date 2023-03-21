049dffe03c736cbeb12862f780a60f4a13dc6863
Maxim Vasilkov <dev@smrt16.com>
2023-03-21 12:24:51 +0200

    lines height

--- components/IndexPage/HeroSection.js
+++ components/IndexPage/HeroSection.js
@@ -10,7 +10,7 @@ export default function HeroSection(props) {
                         <Col className="hero-text" sm={12} lg={6} md={12}>
                             <h1>Join the Future of Referral Marketing with SMRT16</h1>
                             <p>The Decentralized Affiliate Multilevel Marketing Program on the Polygon Blockchain!</p>
-                            <a href="#features" class="btn">Learn More</a>
+                            <a href="#features" class="btn btn-primary btn-lg">Get started</a>
                         </Col>
                         <Col className="hero-image" sm={12} lg={6} md={12}>
                             <Image src="smrt16-hero-image.png" alt="SMRT16 Hero Image" />
