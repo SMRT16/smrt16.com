717629d0c16ed20edc376475c5dd1e892136dc70
hrihinvestments <129267479+HiltonHartnick@users.noreply.github.com>
2024-01-22 15:19:09 +0200

    Update lang.js
    Changed website domain from hrihinvestments.africa to smrt6.co.za

--- data/lang.js
+++ data/lang.js
@@ -21,7 +21,7 @@ export const TheLang = {
         and a decentralized application. 
         All functions are available "as is," and the website does not provide investment advice.
         
-        'Copyright 2023 - HRIH Investments'
+        'Copyright 2024 - HRIH Investments'
         `,
 
         "diclimerBottomline":
@@ -59,7 +59,7 @@ export const TheLang = {
         "howitworks": `
         It works as follows: everyone who has ever bought a SMRT16 token through a DApp or directly using the functions of a smart contract becomes a project partner. 
         It is impossible to buy tokens without specifying a referrer. After the purchase, your wallet address becomes your referral code at the same time. 
-        And if you add hrihinvestments.africa/ and your wallet address, you get your referral link, for example: https://hrihinvestments.africa/0x8B5D5a87b81c068a6e1e9Fa5121e62344209814A.
+        And if you add smrt16.co.za and your wallet address, you get your referral link, for example: https://smrt16.co.za/0x8B5D5a87b81c068a6e1e9Fa5121e62344209814A.
 
         Thus, you become an agent of the project and now you can invite other people to buy SMRT16 tokens. For you - a four-level referral system, in which up to 93.75% of the transaction value is distributed among users directly involved in the transaction. Instant payment of referral bonuses occurs automatically and is guaranteed by a smart contract code on the blockchain.
 
@@ -137,7 +137,7 @@ Partners of the SMTR16 project are highly motivated to engage in marketing - aft
     "itsYourPage": "It's Your page",
     "somebody": "random account",
     "sellTitle": "Your referral link",
-    "sellFooter": `The link consists of the https://hrihinvestments.africa - the domain of the DApp concatenated by '/' with the address of your wallet. 
+    "sellFooter": `The link consists of the https://smrt16.co.za - the domain of the DApp concatenated by '/' with the address of your wallet. 
         Note: that your referral bonuses is limited with your SMRT16 balance.`,
     "copyToClipboard": "Copy Link",
     "copiedToClipboad": "Copied",
@@ -149,7 +149,7 @@ Partners of the SMTR16 project are highly motivated to engage in marketing - aft
     "rememberingRefLink": "Referal link which you visited before",
     "bonusSizeNote": "The size of your referral reward is limited by the size of your smrt16 tokens balance.",
     "burningNote": "Every time you receive your referral reward, a small amount of smrt16 tokens is burned from your balance.",
-    "youNeedToBuy": "In order to have a referral link your wallet needs to be register in the Smart Contract and to make this happen you need to buy any amount of SMRT16 tokens from this wallet. ",
+    "youNeedToBuy": "In order to have a referral link your wallet needs to be registered in the Smart Contract and to make this happen you need to buy any amount of SMRT16 tokens from this wallet. ",
     "yourReferralLinkExpl": "Let your referrals to buy SMRT16 tokens from the page of this link.",
     "PolygonNotConnected": "Your wallet is not connected to Polygon!",
     "opensInNewWindow": "Opens in a new window",
